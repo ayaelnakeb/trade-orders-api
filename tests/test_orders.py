2013c109b67b1192010d0d6f3e2d137d1ba66040
Aya Tarek <ayaelnakeb@adsgency.ai>
2025-02-23 23:24:26 -0800

    trigger pipeline

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -13,4 +13,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the API!"}
+    assert response.json() == {"message": "Welcome to the Trade Orders API!"}
